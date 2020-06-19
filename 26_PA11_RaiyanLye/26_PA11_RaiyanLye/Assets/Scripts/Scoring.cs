1128560ce200b30db02a6a329d8cc6ba26dafa5a
UnicornX1 <64125154+UnicornX1@users.noreply.github.com>
2020-06-19 12:49:38 +0800

    Updated by RaiyanLye

---
+++ 26_PA11_RaiyanLye/26_PA11_RaiyanLye/Assets/Scripts/Scoring.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoring : MonoBehaviour
+{
+    public Text Scoretxt;
+
+    public float score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            score++;
+            Scoretxt.text = "Score : " + score;
+            print("score");
+        }
+    }
+}
---
+++ 26_PA11_RaiyanLye/26_PA11_RaiyanLye/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObjectRed;
+    public GameObject SpawnObjectGreen;
+    public GameObject SpawnObjectBrown;
+
+    public float spawnRate = 2f;
+
+    float nextSpawn = 0f;
+
+    int WhatToSpawn;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            WhatToSpawn = Random.Range(1, 4);
+            //print(WhatToSpawn);
+            nextSpawn = Time.time + spawnRate;
+            switch (WhatToSpawn)
+            {
+                case 1:
+                    PositionY = Random.Range(4, -4f);
+                    this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+                    Instantiate(SpawnObjectRed, transform.position, transform.rotation);
+                    break;
+
+                case 2:
+                    PositionY = Random.Range(4, -4f);
+                    this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+                    Instantiate(SpawnObjectGreen, transform.position, transform.rotation);
+                    break;
+
+                case 3:
+                    PositionY = Random.Range(4, -4f);
+                    this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+                    Instantiate(SpawnObjectBrown, transform.position, transform.rotation);
+                    break;
+            }
+        }
+    }
+}
